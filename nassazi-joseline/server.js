386d39567da57ac2013dd7f56c5b8b78432f0d94
Joseline-Nassazi11 <nassazijoselynn4@gmail.com>
2025-10-17 11:47:17 +0300

    Creating my folders and files

---
+++ nassazi-joseline/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up Pug
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log(" Connected to MongoDB "))
+  .catch((err) => console.error(" MongoDB connection error:", err));
+
+// Routes
+// const routes = require("./routes/index");
+// app.use("/", routes);
+
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
